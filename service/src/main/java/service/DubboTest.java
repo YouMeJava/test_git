674aa82d24c0be0b83535642e1b4b7c6e0ed6a13
gaocunguan <gaocunguan@1164693339@qq.com>
2017-08-27 22:20:42 +0800

    1.0

---
+++ service/src/main/java/service/DubboTest.java
@@ -0,0 +1,5 @@
+package service;
+
+public interface DubboTest {
+	public String test();
+}
---
+++ service/src/main/java/service/impl/DubboTestImpl.java
@@ -0,0 +1,11 @@
+package service.impl;
+
+import service.DubboTest;
+
+public class DubboTestImpl implements DubboTest {
+
+	public String test() {
+		return "我是dubbo服务端。。。。。。";
+	}
+
+}
